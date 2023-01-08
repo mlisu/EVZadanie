3fbd132ea12dc84f6bdb8f3257054ddab7c61d3b
mlisu <lisek_173@wp.pl>
2023-01-08 21:51:59 +0100

    kom

--- zliczanieMinMem.c
+++ zliczanieMinMem.c
@@ -2,16 +2,16 @@
 #include <time.h>
 
 //	funkcje pomocnicze:
-unsigned long long losuj();				// s³owo o losowej liczbie jedynek (rozk³ad normalny)
-unsigned long long losujIle(int ile1);	// s³owo o zadanej liczbie jedynek
+unsigned long long losuj();				// slowo o losowej liczbie jedynek (rozklad normalny)
+unsigned long long losujIle(int ile1);	// slowo o zadanej liczbie jedynek
 
 /*
-	Pod komentarzem funkcja zliczaj¹ca jedynki.
-	Wywo³ywana zawsze z argumentem b = 32. 
+	Pod komentarzem funkcja zliczajaca jedynki.
+	Wywolywana zawsze z argumentem b = 32. 
 
-	Poni¿sza tabela pokazuje z jak¹ liczb¹ porównywaæ
-	argument b (lewa kolumna), ¿eby algorytm dzieli³ s³owo 
-	wejœciowe na s³owa o po¿¹danej iloœci bitów (prawa kolumna):
+	Ponizsza tabela pokazuje z jaka liczb¹ porownywac
+	argument b (lewa kolumna), zeby algorytm dzielil slowo 
+	wejsciowe na slowa o pozadanej ilosci bitow (prawa kolumna):
 
 	b:	  bity:
 	32	- 64
@@ -22,26 +22,26 @@ unsigned long long losujIle(int ile1);	// s³owo o zadanej liczbie jedynek
 	1	- 2
 	0	- 1
 	
-	Przyk³adowo dla parametru = 16, szybciej wykonywany jest 
-	"test szybkoœci 3" (testy w funkcji main) ni¿ dla
+	Przykladowo dla parametru = 16, szybciej wykonywany jest 
+	"test szybkosci 3" (testy w funkcji main) niz dla
 	parametru = 32
 
-	Na koñcu pliku poda³em przyk³ad implementacji która na sztywno
-	dzieli s³owo wejœciowe na 2 s³owa 32 - bitowe (funkcja licz32)
-	oraz implementacji nie dziel¹cej s³owa wejœciowego (licz64)
+	Na koncu pliku podalem przyklad implementacji ktora na sztywno
+	dzieli slowo wejœciowe na 2 slowa 32 - bitowe (funkcja licz32)
+	oraz implementacji nie dzielacej slowa wejsciowego (licz64)
 */
 int licz(unsigned long long liczba, int b)
 {
 	if(!liczba)
 		return 0;
-	else if(b == 32){ //mo¿liwa zmiana parametru, zmieniæ te¿ poni¿ej
+	else if(b == 32){ //mozliwa zmiana parametru, zmienic tez ponizej
 		while(liczba){
 			b += liczba & 1;
 			liczba >>= 1;
 		}
-		return b - 32; //wpisaæ ten sam parametr, co powy¿ej
+		return b - 32; //wpisac ten sam parametr, co powyzej
 	}else
-		//podzia³ s³owa wejœciowego na wzór drzewa binarnego:
+		//podzial slowa wejsciowego na wzor drzewa binarnego:
 		return licz(liczba >> b, b >> 1) + licz(liczba & 0xffffffff >> 32 - b, b >> 1);
 		
 }
@@ -53,30 +53,30 @@ main()
 	clock_t t;
 	srand(time(NULL));
 	
-	//	test poprawnoœci:
+	//	test poprawnosci:
 	for(k = 0; k < 65; k++){
 		num = losujIle(k);
 		if(licz(num, 32) != k){
-			printf("b³¹d dla liczby %d\n", k);
+			printf("blad dla liczby %d\n", k);
 			return;
 		}		
 	}
 	printf("test poprawnosci: ok\n");
 	
 	/*	
-		w poni¿szych testach zmienna k jest iterowana ró¿n¹ iloœæ razy,
-		bo wyniki s³u¿¹ do porównania ró¿nych algorytmów a nie wyników
-		jednego algorytmu dla ró¿nych testów
+		w ponizszych testach zmienna k jest iterowana rozn¹ ilosc razy,
+		bo wyniki sluza do porownania roznych algorytmow a nie wynikow
+		jednego algorytmu dla roznych testow
 	*/
 	
-	//	test szybkoœci dla losowego slowa:
+	//	test szybkosci dla losowego slowa:
 	t = clock();
 	for(k = 0; k < 0x1ffffff; k++)
 		licz(losuj(), 32);
 	t = clock() - t;
 	printf("test szybkosci 1 -> liczba cykli: %u\n", t);
 	
-	//	test szybkoœci dla slowa bitowego o losowej (rozklad jednostajny) liczbie jedynek:
+	//	test szybkosci dla slowa bitowego o losowej (rozklad jednostajny) liczbie jedynek:
 	t = clock();
 	for(k = 0; k < 0xffffff; k++){
 		num = losujIle(rand() % 65);
@@ -142,7 +142,7 @@ int licz64(unsigned long long liczba)
 	return ile;
 }
 
-int podLicz(unsigned long liczba) //funkcja wywo³ywana przez licz32
+int podLicz(unsigned long liczba) //funkcja wywolywana przez licz32
 {
 	int ile = 0;
 	while(liczba){
